0536d720235b4b0b79e1114bfb15495d8879c485
johanadiwimbanu <johanadiwimbanu@gmail.com>
2025-03-21 12:32:22 +0700

    feat: navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
-<script setup></script>
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <Navbar />
 </template>
 
 <style scoped></style>
